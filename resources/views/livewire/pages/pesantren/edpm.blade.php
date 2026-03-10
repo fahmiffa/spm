6fc840472e85d5553a86cce16ab505bbcb26f592
fahmiffa <faisol.ajifa@gmail.com>
2026-03-10 20:42:19 +0700

    loading

--- resources/views/livewire/pages/pesantren/edpm.blade.php
+++ resources/views/livewire/pages/pesantren/edpm.blade.php
@@ -410,11 +410,15 @@ new #[Layout('layouts.app')] class extends Component {
                                 <span>Simpan Permanen EDPM</span>
                             </button>
                             @else
-                            <button type="button" @click="validateAndNext($wire)"
+                            <button type="button" @click="validateAndNext($wire)" wire:loading.attr="disabled"
                                 class="w-full md:w-auto bg-indigo-600 text-white font-bold py-3 px-8 rounded-2xl flex items-center justify-center gap-2 transition-all">
                                 <span>Selanjutnya</span>
-                                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <svg wire:loading.remove wire:target="nextStep" class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                                </svg>
+                                <svg wire:loading wire:target="nextStep" class="animate-spin h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
                             </button>
                             @endif
