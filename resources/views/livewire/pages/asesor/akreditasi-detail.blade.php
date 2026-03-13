af5e27de991d6b5377b0d1826e6151e808c154c6
fahmiffa <faisol.ajifa@gmail.com>
2026-03-13 12:57:15 +0700

    text

--- resources/views/livewire/pages/asesor/akreditasi-detail.blade.php
+++ resources/views/livewire/pages/asesor/akreditasi-detail.blade.php
@@ -362,8 +362,11 @@
                                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
                                 </svg>
                                 <div>
-                                    <h4 class="text-sm font-bold text-yellow-900">Data Sedang Diverifikasi</h4>
-                                    <p class="text-xs text-yellow-700 mt-1">Assessment telah diselesaikan dan saat ini sedang dalam proses verifikasi oleh admin. Data tidak dapat diubah.</p>
+                                    <h4 class="text-base font-bold text-yellow-900">Data Sedang Diverifikasi</h4>
+                                    <p class="text-sm text-yellow-700 mt-1">Assessment telah diselesaikan dan saat ini sedang dalam proses verifikasi oleh admin. Data tidak dapat diubah.</p>
+                                    @if ($this->asesorTipe == 1)
+                                    <p class="text-sm text-yellow-700 mt-3">Nilai NK baru bisa diinputkan setelah NA1 DAN NA2 terisi/selesai diinputkan</p>
+                                    @endif
                                 </div>
                             </div>
                         </div>
